3d33a16f28861216ed09810a32080d1d7a8c9c0e
Anton Vlasik <snumber29@gmail.com>
2018-10-30 16:23:12 +0300

    Added interface and queue implementation

--- scripts/Generics.ts
+++ scripts/Generics.ts
@@ -1,16 +1,40 @@
-class Queue<T> {
+interface ICollection<T> {
+    toString: () => string;
+    isEmpty: () => boolean;
+}
+
+class Queue<T> implements ICollection<T> {
     private data: Array<T> = [];
 
+    private index = 0;
+
     public enqueue(item: T): void {
-        this.data.push(item);
+        this.data[this.index++] = item;
     }
 
     public dequeue(): T {
-        return this.data.shift();
+        if (this.isEmpty()) {
+            return;
+        }
+        const item = this.data[0];
+        for (let i = 0; i < this.index; i += 1) {
+            this.data[i] = this.data[i + 1];
+        }
+        this.data.length -= 1;
+        this.index -= 1;
+        return item;
+    }
+
+    public isEmpty() {
+        return !this.data.length;
+    }
+
+    public toString() {
+        return this.data.toString();
     }
 }
 
-class Stack<T> {
+class Stack<T> implements ICollection<T> {
     private data: Array<T> = [];
 
     private index = 0;
@@ -20,7 +44,7 @@ class Stack<T> {
     }
 
     public pop(): T {
-        if(this.isEmpty()) {
+        if (this.isEmpty()) {
             return;
         }
         const item = this.data[--this.index];
@@ -38,3 +62,4 @@ class Stack<T> {
 }
 
 const stack = new Stack<number>();
+const queue = new Queue<string>();
