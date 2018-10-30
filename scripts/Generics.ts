a67340ff029047046cd229ba2c31114139abac2d
Anton Vlasik <snumber29@gmail.com>
2018-10-30 20:23:11 +0300

    getters added

--- scripts/Generics.ts
+++ scripts/Generics.ts
@@ -1,63 +1,66 @@
 interface ICollection<T> {
     toString: () => string;
     isEmpty: () => boolean;
+    size: number
 }
 
 class Queue<T> implements ICollection<T> {
-    private data: Array<T> = [];
+    private _data: Array<T> = [];
+    private _index = 0;
 
-    private index = 0;
+    get size() {return this._index;}
 
     public enqueue(item: T): void {
-        this.data[this.index++] = item;
+        this._data[this._index++] = item;
     }
 
     public dequeue(): T {
         if (this.isEmpty()) {
             return;
         }
-        const item = this.data[0];
-        for (let i = 0; i < this.index; i += 1) {
-            this.data[i] = this.data[i + 1];
+        const item = this._data[0];
+        for (let i = 0; i < this._index; i += 1) {
+            this._data[i] = this._data[i + 1];
         }
-        this.data.length -= 1;
-        this.index -= 1;
+        this._data.length -= 1;
+        this._index -= 1;
         return item;
     }
 
     public isEmpty() {
-        return !this.data.length;
+        return !this._data.length;
     }
 
     public toString() {
-        return this.data.toString();
+        return this._data.toString();
     }
 }
 
 class Stack<T> implements ICollection<T> {
-    private data: Array<T> = [];
+    private _data: Array<T> = [];
+    private _index = 0;
 
-    private index = 0;
+    public get size() {return this._index;}
 
     public push(item: T): void {
-        this.data[this.index++] = item;
+        this._data[this._index++] = item;
     }
 
     public pop(): T {
         if (this.isEmpty()) {
             return;
         }
-        const item = this.data[--this.index];
-        this.data.length -= 1;
+        const item = this._data[--this._index];
+        this._data.length -= 1;
         return item;
     }
 
     public isEmpty() {
-        return !this.data.length;
+        return !this._data.length;
     }
 
     public toString() {
-        return this.data.toString();
+        return this._data.toString();
     }
 }
 
