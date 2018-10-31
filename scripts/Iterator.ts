f105f17dcc4c52c11748b8676803fa138b9d820a
Anton Vlasik <snumber29@gmail.com>
2018-10-31 22:23:37 +0300

    Interfaces for iterabeles

--- scripts/Iterator.ts
+++ scripts/Iterator.ts
@@ -1,19 +1,38 @@
-const range = function (from, to) {
+interface IteratorResult<T> {
+    done: boolean;
+    value: T;
+}
+
+interface Iterator<T> {
+    next(value?: any): IteratorResult<T>;
+    return?(value?: any): IteratorResult<T>;
+    throw?(e?: any): IteratorResult<T>;
+}
+
+interface Iterable<T> {
+    from?: any,
+    to?: any,
+    [Symbol.iterator](): Iterator<T>
+}
+
+function range(from: number, to: number): Iterable<number> {
     return {
         from,
         to,
-        [Symbol.iterator]: function () {
-            let current = this.from;
-            let last = this.to;
+        [Symbol.iterator](): Iterator<number> {
+            let current: number = this.from;
+            let last: number = this.to;
 
             return {
-                next: () => current <= last
-                    ? {done: false, value: current++}
-                    : {done: true, value: undefined}
+                next(): IteratorResult<number> {
+                    return current <= last
+                        ? {done: false, value: current++}
+                        : {done: true, value: undefined}
+                }
             }
         }
     };
-};
+}
 
 for (let num of range(4, 9)) {
     console.log(num);
