c7abef6b15428b485d2fff51b0709381b9cb1ce2
Anton Vlasik <snumber29@gmail.com>
2018-10-31 23:38:04 +0300

    measure performace altered

--- scripts/Measure_Performance.ts
+++ scripts/Measure_Performance.ts
@@ -2,12 +2,19 @@
  * @name measurePerformance
  * A wrapper to measure time spent for function execution
  * @example measurePerformance(1000, calculateSomethingBig, param_1, param_2, ..., param_N)
+ * First parameter can be omitted, then function is called 1 time.
+ * @example measurePerformance(calculateSomethingBig, param_1, param_2, ..., param_N)
  */
-function measurePerformance(times: number, func: Function, ...args): void {
-    let result: any;
+function measurePerformance(...args): void {
+    let result;
+    const isTimesOmitted = typeof args[0] === 'function';
+    const times = isTimesOmitted ? 1 : args[0];
+    const func = isTimesOmitted ? args[0] : args[1];
     const start = performance.now();
-    for (let i: number = 0; i < times; i++) {
-        result = func(...args)
+    for (let i = 0; i < times; i++) {
+        result = func(...args.slice(isTimesOmitted ? 1 : 2))
     }
-    console.log((performance.now() - start).toFixed(2));
+    console.log(result);
+    console.log(`Time: ${parseFloat((performance.now() - start).toFixed(2))} ms`);
+    console.log('------------------------');
 }
