ea4ac26d2c1d3514711db28b0f84480501ae071a
Anton Vlasik <snumber29@gmail.com>
2020-04-28 12:23:40 +0300

    add leadingZeros version 2

--- scripts/SetLeadingZeros.ts
+++ scripts/SetLeadingZeros.ts
@@ -1,3 +1,9 @@
-const setLeadingZeros = (value, digitsNumber = 2) =>
+const setLeadingZerosNumber = (value, digitsNumber = 2) =>
     [...new Array(digitsNumber - 1)]
         .reduce((acc, current, i) => value < Math.pow(10, i + 1) ? `0${acc}` : `${acc}`, value);
+
+
+const seLeadingZerosString = (value, digitsNumber = 2) => {
+    const repeatCount = digitsNumber - value.toString().length;
+    return `${"0".repeat(repeatCount < 0 ? 0 : repeatCount)}${value}`;
+};
