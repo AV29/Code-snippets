408bce6c7ed2a6360629c1fff32ab2dd80018f14
Anton Vlasik <snumber29@gmail.com>
2020-10-20 10:11:06 +0300

    refactor curry

--- scripts/Curry.ts
+++ scripts/Curry.ts
@@ -1,15 +1,17 @@
-// var f = curry(fn, 1, 2, 3); ... f(4, 5);
+// const fn = (a, b, c, d, e) => a + b + c + d + e;
 
-// var f = curry(fn, 1); ... f(2, 3, 4, 5);
+// var f = fn.curry(1, 2, 3); ... f(4, 5);
 
-// var f = curry(fn, 1); ... f(2)(3)(4)(5);
+// var f = fn.curry(1); ... f(2, 3, 4, 5);
+
+// var f = fn.curry(1); ... f(2)(3)(4)(5);
 
 interface Function {
     curry(params: any): Function
 }
 
 Function.prototype.curry = function(...args) {
-    return args.length >= this.length
-        ? this(...args)
-        : (...nextArgs) => this.curry(...args, ...nextArgs)
+    return args.length < this.length
+        ? (...nextArgs) => this.curry(...args, ...nextArgs)
+        : this(...args)
 };
